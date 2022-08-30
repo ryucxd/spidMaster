729255e87cbb135b663b72c2c3a0f1601ef33f6a
ryucxd <ryucxd@gmail.com>
2022-08-30 11:00:01 +0100

    Add project files.

---
+++ spidMaster/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace spidMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("spid_master", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+
+                }
+                conn.Close();
+            }
+        }
+    }
+}
